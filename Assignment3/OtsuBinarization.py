cc57e39852516e084e1fde07b78d2a54b30f5e29
Paul-p03 <PPatullo03@gmail.com>
2025-03-11 16:21:37 -0400

    Used matplotlib instead of tkinter and plotted the Otsu threshold and gaussian blurred Otsu threshold

--- Assignment3/OtsuBinarization.py
+++ Assignment3/OtsuBinarization.py
@@ -1,30 +1,25 @@
 import cv2 as cv
-import tkinter as tk
-from tkinter import Frame
-from PIL import Image, ImageTk
-
-root = tk.Tk()
-root.title("Image Segmentation")
-root.configure(background = "white")
-root.geometry("500x500+475+150")
-
-frame = Frame(root, bg = "black")
-frame.pack(pady = 10)
+import numpy as np
+from matplotlib import pyplot as plt
 
 sittingImage = r"C:\Users\ppatu\Computer-Vision-Assignments\Computer-Vision-Assignments\Assignment3\Images\manSitting.png"
 standingImage = r"C:\Users\ppatu\Computer-Vision-Assignments\Computer-Vision-Assignments\Assignment3\Images\manStanding.png"
 
 image1 = cv.imread(sittingImage, cv.IMREAD_GRAYSCALE)
 image2 = cv.imread(standingImage, cv.IMREAD_GRAYSCALE)
-image1_PIL = Image.fromarray(image1)
-image1_PIL = image1_PIL.resize((250, 250))
-photo = ImageTk.PhotoImage(image1_PIL)
 
-label = tk.Label(frame, image=photo, bg="white")
-label.image = photo
-label.pack()
+ret1, th1 = cv.threshold(image1, 127,255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+blur = cv.GaussianBlur(image1, (5,5), 0)
+ret2, th2 = cv.threshold(blur, 127,255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 
+images = [image1, th1, image1, th2]
+titles = ['Original Image 1', 'Otsu Threshold 1', 'Original Image 2', 'Otsu Threshold 2']
 
-root.mainloop()
+for i in range(len(images)):
+    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+    plt.title(titles[i])  # Add corresponding title
+    plt.xticks([])  # Remove x-axis ticks
+    plt.yticks([])  # Remove y-axis ticks
 
+plt.show()
 
